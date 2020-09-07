e0c34e2709b3edaa90034181326a4f902e1f642b
sudhanshutiwary <sudhanshu@abacusdesk.co.in>
2020-09-07 15:10:37 +0530

    tem del

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,170 +1,170 @@
-const path = require('path')
+// const path = require('path')
 
-// Testing Post this Function
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
+// // Testing Post this Function
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
 
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-  return graphql(
-    `
-        {
-            allContentfulPost {
-              edges {
-                node {
-                  title
-                  slug
-                  content {
-                    childContentfulRichText {
-                      html
-                    }
-                  }
-                  image {
-                    fluid {
-                      src
-                    }
-                  }
-                }
-              }
-            }
-          }
-          `
+//   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+//   return graphql(
+//     `
+//         {
+//             allContentfulPost {
+//               edges {
+//                 node {
+//                   title
+//                   slug
+//                   content {
+//                     childContentfulRichText {
+//                       html
+//                     }
+//                   }
+//                   image {
+//                     fluid {
+//                       src
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//           `
 
-  ).then(result => {
-    if (result.errors) {
-      throw (result.errors)
-    }
+//   ).then(result => {
+//     if (result.errors) {
+//       throw (result.errors)
+//     }
 
-    const posts = result.data.allContentfulPost.edges
+//     const posts = result.data.allContentfulPost.edges
 
-    posts.forEach((post, index) => {
-      createPage({
-        path: post.node.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.slug
-        },
-      })
-    })
-  })
+//     posts.forEach((post, index) => {
+//       createPage({
+//         path: post.node.slug,
+//         component: blogPost,
+//         context: {
+//           slug: post.node.slug
+//         },
+//       })
+//     })
+//   })
 
-}
-
-
-// logo design page
+// }
 
 
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-
-  const Pages = path.resolve(`./src/templates/logo-page-design.js`)
-  const AboutPages = path.resolve(`./src/templates/about-page-design.js`)
-  const blogPost = path.resolve(`./src/templates/blog-post.js`)
-
-  return graphql(
-    `
-        {
-            allContentfulPage(filter: {id: {ne: "null"}}){
-              edges {
-                node {
-                  title
-                  slug
-                  image {
-                    file {
-                      url
-                    }
-                  }
-                  subHeading
-                  content {
-                    childContentfulRichText {
-                      html
-                    }
-                  }
-                  footerContent {
-                    footerContent
-                  }
-                  footerSubContent
-                }
-              }
-            }
-            allContentfulAboutPage {
-              edges {
-                node {
-                  title
-                  slug
-                  image {
-                    file {
-                      url
-                    }
-                  }
-                  subHeading
-                  content {
-                    childContentfulRichText {
-                      html
-                    }
-                  }
-                }
-              }
-            }
-            allContentfulPost {
-              edges {
-                node {
-                  title
-                  slug
-                  content {
-                    childContentfulRichText {
-                      html
-                    }
-                  }
-                  image {
-                    fluid {
-                      src
-                    }
-                  }
-                }
-              }
-            }
-          }
-          
-          `
-
-  ).then(result => {
-    if (result.errors) {
-      throw (result.errors)
-    }
+// // logo design page
 
 
-    const pages = result.data.allContentfulPage.edges
-    const aboutpages = result.data.allContentfulAboutPage.edges
-    const posts = result.data.allContentfulPost.edges
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+
+//   const Pages = path.resolve(`./src/templates/logo-page-design.js`)
+//   const AboutPages = path.resolve(`./src/templates/about-page-design.js`)
+//   const blogPost = path.resolve(`./src/templates/blog-post.js`)
+
+//   return graphql(
+//     `
+//         {
+//             allContentfulPage(filter: {id: {ne: "null"}}){
+//               edges {
+//                 node {
+//                   title
+//                   slug
+//                   image {
+//                     file {
+//                       url
+//                     }
+//                   }
+//                   subHeading
+//                   content {
+//                     childContentfulRichText {
+//                       html
+//                     }
+//                   }
+//                   footerContent {
+//                     footerContent
+//                   }
+//                   footerSubContent
+//                 }
+//               }
+//             }
+//             allContentfulAboutPage {
+//               edges {
+//                 node {
+//                   title
+//                   slug
+//                   image {
+//                     file {
+//                       url
+//                     }
+//                   }
+//                   subHeading
+//                   content {
+//                     childContentfulRichText {
+//                       html
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//             allContentfulPost {
+//               edges {
+//                 node {
+//                   title
+//                   slug
+//                   content {
+//                     childContentfulRichText {
+//                       html
+//                     }
+//                   }
+//                   image {
+//                     fluid {
+//                       src
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//           }
+
+//           `
+
+//   ).then(result => {
+//     if (result.errors) {
+//       throw (result.errors)
+//     }
 
 
-    pages.forEach((page, index) => {
-      createPage({
-        path: page.node.slug,
-        component: Pages,
-        context: {
-          slug: page.node.slug
-        },
-      })
-    })
-    aboutpages.forEach((about, index) => {
-      createPage({
-        path: about.node.slug,
-        component: AboutPages,
-        context: {
-          slug: about.node.slug
-        },
-      })
-    })
-    posts.forEach((post, index) => {
-      createPage({
-        path: post.node.slug,
-        component: blogPost,
-        context: {
-          slug: post.node.slug
-        },
-      })
-    })
-  })
+//     const pages = result.data.allContentfulPage.edges
+//     const aboutpages = result.data.allContentfulAboutPage.edges
+//     const posts = result.data.allContentfulPost.edges
 
-}
+
+//     pages.forEach((page, index) => {
+//       createPage({
+//         path: page.node.slug,
+//         component: Pages,
+//         context: {
+//           slug: page.node.slug
+//         },
+//       })
+//     })
+//     aboutpages.forEach((about, index) => {
+//       createPage({
+//         path: about.node.slug,
+//         component: AboutPages,
+//         context: {
+//           slug: about.node.slug
+//         },
+//       })
+//     })
+//     posts.forEach((post, index) => {
+//       createPage({
+//         path: post.node.slug,
+//         component: blogPost,
+//         context: {
+//           slug: post.node.slug
+//         },
+//       })
+//     })
+//   })
+
+// }
